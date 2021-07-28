d2752b151940091115b4c54fbd9cd32514f4bf20
ManuDev03 <manuprajan03@gmail.com>
2021-07-28 18:59:29 +0530

    Reactive-form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -14,9 +14,9 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     this.signupform = new FormGroup({
-      'username': new FormControl(''),
-      'email': new FormControl(''),
-      'gender': new FormControl('')
+      'username': new FormControl(null,Validators.required),
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'gender': new FormControl('male')
 
     })
   }
